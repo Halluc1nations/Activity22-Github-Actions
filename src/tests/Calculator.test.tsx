937f86c126670a6a6e1789f4404271569c1738a9
Halluc1nations <Blaine.Bishop@comcast.net>
2025-05-01 21:43:18 -0400

    test edit add h2

--- src/tests/Calculator.test.tsx
+++ src/tests/Calculator.test.tsx
@@ -1,26 +1,29 @@
-import pretty from 'pretty';
-import { render } from '@testing-library/react';
-import Calculator from '../components/Calculator';
+import pretty from "pretty";
+import { render } from "@testing-library/react";
+import Calculator from "../components/Calculator";
 
-describe('Calculator', () => {
-  it('should contain the expected text', () => {
+describe("Calculator", () => {
+  it("should contain the expected text", () => {
     render(<Calculator />);
 
-    const itemElement = document.querySelector('h2');
+    const itemElement = document.querySelector("h2");
 
     if (itemElement) {
-      expect(itemElement.textContent).toBe('Simple Calculator');  
+      expect(itemElement.textContent).toBe("Simple Calculator");
     }
-    
   });
 
-  it('should match snapshot', () => {
+  it("should match snapshot", () => {
     render(<Calculator />);
 
-    const calculatorElement = document.querySelector('.calculator');
+    const itemElement = document.querySelector("h2");
+
+    const calculatorElement = document.querySelector(".calculator");
 
     if (calculatorElement) {
-      expect(pretty(calculatorElement.innerHTML)).toMatchSnapshot();
+      expect(
+        pretty(itemElement, calculatorElement.innerHTML)
+      ).toMatchSnapshot();
     }
   });
 });
