1cdaf8454805e1f1aa216141760786ac38a6fa0f
Halluc1nations <Blaine.Bishop@comcast.net>
2025-05-01 21:52:17 -0400

    h2 add correct

--- src/tests/Calculator.test.tsx
+++ src/tests/Calculator.test.tsx
@@ -1,29 +1,26 @@
-import pretty from "pretty";
-import { render } from "@testing-library/react";
-import Calculator from "../components/Calculator";
+import pretty from 'pretty';
+import { render } from '@testing-library/react';
+import Calculator from '../components/Calculator';
 
-describe("Calculator", () => {
-  it("should contain the expected text", () => {
+describe('Calculator', () => {
+  it('should contain the expected text', () => {
     render(<Calculator />);
 
-    const itemElement = document.querySelector("h2");
+    const itemElement = document.querySelector('h2');
 
     if (itemElement) {
-      expect(itemElement.textContent).toBe("Simple Calculator");
+      expect(itemElement.textContent).toBe('Simple Calculator');  
     }
+    
   });
 
-  it("should match snapshot", () => {
+  it('should match snapshot', () => {
     render(<Calculator />);
 
-    const itemElement = document.querySelector("h2");
-
-    const calculatorElement = document.querySelector(".calculator");
+    const calculatorElement = document.querySelector('.calculator');
 
     if (calculatorElement) {
-      expect(
-        pretty(itemElement, calculatorElement.innerHTML)
-      ).toMatchSnapshot();
+      expect(pretty(calculatorElement.innerHTML)).toMatchSnapshot();
     }
   });
 });
